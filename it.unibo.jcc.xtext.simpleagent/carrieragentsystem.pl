bbe53d171229d250bfdeb587d0123da78bdd04ed
Jean Claude Correale <jeanclaude.correale@studio.unibo.it>
2019-11-10 01:43:59 +0100

    Updated .gitignore

--- it.unibo.jcc.xtext.simpleagent/carrieragentsystem.pl
+++ it.unibo.jcc.xtext.simpleagent/carrieragentsystem.pl
@@ -3,8 +3,10 @@
 %====================================================================================
 mqttBroker("localhost", "1883").
 context(ctxcarrieragent, "localhost",  "MQTT", "0" ).
+context(ctxdummyformovebehavior, "otherresourcelocalhost",  "MQTT", "0" ).
 context(ctxdummyforgotobehavior, "otherresourcelocalhost",  "MQTT", "0" ).
 context(ctxdummyforroomstate, "otherresourcelocalhost",  "MQTT", "0" ).
- qactor( gotobehavior, ctxdummyforgotobehavior, "external").
+ qactor( movebehavior, ctxdummyformovebehavior, "external").
+  qactor( gotobehavior, ctxdummyforgotobehavior, "external").
   qactor( roomstate, ctxdummyforroomstate, "external").
   qactor( carrierbehavior, ctxcarrieragent, "it.unibo.carrierbehavior.Carrierbehavior").
